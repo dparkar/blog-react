db19f01f0f73f068c3806e197da9d247bb169aa1
Dhawal Parkar <dhawalparkar@gmail.com>
2017-08-20 19:13:02 -0700

    using react-markdown and moving divs around

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { Router, browserHistory, Route, Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
-import MarkdownRenderer from 'react-markdown-renderer';
+import ReactMarkdown from 'react-markdown';
 
-const markdown = '# This is a H1  \n## This is a H2  \n###### This is a H6';
+var input = '# This is a header\n\nAnd this is a paragraph';
 
 const Page = ({ title }) =>
   <div className="App">
     <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <h2>
         {title}
       </h2>
@@ -17,40 +16,35 @@ const Page = ({ title }) =>
     <p className="App-intro">
       This is the {title} page.
     </p>
-    <p>
+    <div className="App-navigation">
       <Link to="/">Home</Link>
-    </p>
-    <p>
+      <br />
       <Link to="/about">About</Link>
-    </p>
-    <p>
-      <Link to="/settings">Settings</Link>
-    </p>
-    <MarkdownRenderer markdown={markdown} />
+    </div>
+    <ReactMarkdown source={input} />
+
+    <div className="App-footer">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
   </div>;
 
 const Home = props => <Page title="Home" />;
-
 const About = props => <Page title="About" />;
 
-const Settings = props => <Page title="Settings" />;
-
 class App extends Component {
+  componentWillMount() {
+    const url =
+      'https://api.github.com/repos/dparkar/blog-react/contents/README.md';
+    fetch(url)
+      .then(response => response) // i've tried with and without .json()
+      .then(body => console.log(body));
+    // this logs the same response
+  }
   render() {
     return (
-      // <div className="App">
-      //   <div className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <h2>Welcome to React</h2>
-      //   </div>
-      //   <p className="App-intro">
-      //     To get started, edit <code>src/App.js</code> and save to reload.
-      //   </p>
-      // </div>
       <Router history={browserHistory}>
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/settings" component={Settings} />
       </Router>
     );
   }
