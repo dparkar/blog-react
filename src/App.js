09671f54fcbfb8600d7f7f640d5239fa2611a14c
Dhawal Parkar <dhawalparkar@gmail.com>
2017-08-29 01:12:46 -0700

    learning state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Router, browserHistory, Route, Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 import ReactMarkdown from 'react-markdown';
+var base64 = require('js-base64').Base64;
 
 var input = '# This is a header\n\nAnd this is a paragraph';
 
@@ -21,6 +22,7 @@ const Page = ({ title }) =>
       <br />
       <Link to="/about">About</Link>
     </div>
+
     <ReactMarkdown source={input} />
 
     <div className="App-footer">
@@ -32,20 +34,45 @@ const Home = props => <Page title="Home" />;
 const About = props => <Page title="About" />;
 
 class App extends Component {
-  componentWillMount() {
+  constructor(props) {
+    console.log('constructor');
+    super(props);
     const url =
-      'https://api.github.com/repos/dparkar/blog-react/contents/README.md';
+      'https://api.github.com/repos/dparkar/blog-react/contents/content/2017-08-20_19:27.md';
     fetch(url)
-      .then(response => response) // i've tried with and without .json()
-      .then(body => console.log(body));
-    // this logs the same response
+      .then(response => response)
+      //.then(body => input = base64.decode(body));
+      .then(body => (this.state = { body: body }));
+    this.state = { date: new Date() };
+  }
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
   }
   render() {
+    console.log('render');
     return (
-      <Router history={browserHistory}>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </Router>
+      <div>
+        {/* <Router history={browserHistory}>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Router  > */}
+        <p>
+          It is {this.state.date.toLocaleTimeString()}.
+        </p>
+      </div>
     );
   }
 }
