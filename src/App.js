2f71b353e5d902b537dbec1b0fc534fb6c8c113f
Dhawal Parkar <dhawalparkar@gmail.com>
2017-09-05 00:22:31 -0700

    fixing menu scroll bar issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Router, browserHistory, Route } from 'react-router';
 import Page from './Page.js';
-import './App.css';
 
 const Logs = props => <Page title="Logs" />;
 const About = props => <Page title="About" />;
--- src/Page.js
+++ src/Page.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { pushRotate as Menu } from 'react-burger-menu';
 import logo from './logo.svg';
+import './Page.css';
 
 export default class Page extends Component {
   render() {
     var title = this.props.title;
     return (
-      <div className="App" id="outer-container">
+      <div className="Page" id="outer-container">
         <Menu pageWrapId={'page-wrap'} outerContainerId={'outer-container'}>
           <a id="logs" className="menu-item" href="/">
             Logs
@@ -21,14 +22,14 @@ export default class Page extends Component {
               {title} | Dhawal Parkar
             </title>
           </head>
-          <div className="App-header">
+          <div className="Page-header">
             {title === 'Logs' ? <label> Dhawal Parkar logs ...</label> : title}
           </div>
-          <p className="App-intro">
+          <p className="Page-content">
             This is the {title} page.
           </p>
-          <div className="App-footer">
-            <img src={logo} className="App-logo" alt="logo" />
+          <div className="Page-footer">
+            <img src={logo} className="Page-logo" alt="logo" />
           </div>
         </main>
       </div>
