b84ddc87bbaa00fea2c34f46802ddd1ef6b645c5
Dhawal Parkar <dhawalparkar@gmail.com>
2017-09-15 00:09:33 -0700

    correcting timeline

--- src/logs.jsx
+++ src/logs.jsx
@@ -4,20 +4,20 @@ import './logs.css';
 
 export default class Logs extends Component {
   render() {
-    var event1 = { key: 1, details: 'this is the first event' };
-    var event2 = { key: 2, details: 'this is the second event' };
-    var event3 = { key: 3, details: 'this is the third event' };
-    var event4 = { key: 4, details: 'this is the fourth event' };
+    var event1 = { id: 1, details: 'this is the first event' };
+    var event2 = { id: 2, details: 'this is the second event' };
+    var event3 = { id: 3, details: 'this is the third event' };
+    var event4 = { id: 4, details: 'this is the fourth event' };
     var events = [event1, event2, event3, event4];
 
     return (
       <div className="Logs">
         <Chronology type="vertical">
-          {events.map(event =>
-            <div>
-              <div class="marker" />
-              <div class="event">
-                {event.details}
+          {events.map((event, i) =>
+            <div key={event.id}>
+              <div className="marker" />
+              <div className="event event-vertical">
+                {`#${i} ${event.details}`}
               </div>
             </div>
           )}
