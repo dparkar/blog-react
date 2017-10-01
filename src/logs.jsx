c1e0ef281e11f16623789e19272e3feac644302b
Dhawal Parkar <dhawalparkar@gmail.com>
2017-10-01 00:25:33 -0700

    successfully fetching from github

--- src/logs.jsx
+++ src/logs.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import { TrackedComponent } from 'react-appinsights';
+import GitHub from 'github-api';
 import './logs.css';
 
 export default class Logs extends TrackedComponent {
+  constructor(props) {
+    super(props);
+    var gh = new GitHub();
+    var repoName = gh._getFullName('dparkar', 'blog-react');
+    var repo = gh.getRepo('dparkar', 'blog-react');
+    repo.getDetails((err, data) => {
+      if (err) {
+        console.log(err); // we can't have the data, for some reason
+        return;
+      }
+      console.log(data);
+      this.setState({ id: data.id });
+    });
+    // Update state
+    this.state = {
+      repoName: repoName,
+      id: 'fetching id ...'
+    };
+  }
+
   render() {
     return (
       <div className="Logs">
+        {/*get all markdowns from github folder*/}
+        {/*foreach markdown use component to display*/}
+        <p>
+          {'_getFullName : ' + this.state.repoName}
+        </p>
+        <p>
+          {'repo.details.id : ' + this.state.id}
+        </p>
         <p>work in progress ...</p>
         <img
           src="https://i.makeagif.com/media/10-27-2015/_jDzHB.gif"
