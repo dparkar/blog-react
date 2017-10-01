ad44b75977b96b14ebf32b96e84eda7235f403bb
Dhawal Parkar <dhawalparkar@gmail.com>
2017-10-01 01:50:52 -0700

    successfully displaying first markdown

--- src/logs.jsx
+++ src/logs.jsx
@@ -1,40 +1,50 @@
 import React from 'react';
 import { TrackedComponent } from 'react-appinsights';
 import GitHub from 'github-api';
+import ReactMarkdown from 'react-markdown';
 import './logs.css';
 
 export default class Logs extends TrackedComponent {
   constructor(props) {
     super(props);
     var gh = new GitHub();
-    var repoName = gh._getFullName('dparkar', 'blog-react');
+    // get the repo
     var repo = gh.getRepo('dparkar', 'blog-react');
-    repo.getDetails((err, data) => {
-      if (err) {
-        console.log(err); // we can't have the data, for some reason
+    // get the folder contents
+    repo.getContents('master', 'content', true, (err1, files) => {
+      if (err1) {
+        console.log(err1); // we can't get the data, for some reason
         return;
       }
-      console.log(data);
-      this.setState({ id: data.id });
+      // go through each file in the folder
+      files.forEach(function(file) {
+        repo.getContents(
+          'master',
+          'content/' + file.name,
+          true,
+          (err2, content) => {
+            if (err2) {
+              console.log(err2); // we can't have the data, for some reason
+              return;
+            }
+            console.log(content);
+            this.setState({ logs: content });
+          }
+        );
+      }, this);
     });
     // Update state
     this.state = {
-      repoName: repoName,
-      id: 'fetching id ...'
+      //repoName: repoName,
+      id: 'fetching id ...',
+      logs: 'fetching logs ...'
     };
   }
 
   render() {
     return (
       <div className="Logs">
-        {/*get all markdowns from github folder*/}
-        {/*foreach markdown use component to display*/}
-        <p>
-          {'_getFullName : ' + this.state.repoName}
-        </p>
-        <p>
-          {'repo.details.id : ' + this.state.id}
-        </p>
+        <ReactMarkdown source={this.state.logs} />
         <p>work in progress ...</p>
         <img
           src="https://i.makeagif.com/media/10-27-2015/_jDzHB.gif"
