14c3632c53f86e49d208a921cef7e2bc16433092
Dhawal Parkar <dhawalparkar@gmail.com>
2017-10-03 04:28:20 -0700

    adding multiple posts capability (#9)
    * adding logs metadata and second post
    
    * adding file extensions
    
    * adding links to markdown instead of rendering
    
    * parameterizing link
    
    * using react-collapse
    
    * trying events
    
    * cleaning up mapping
    
    * adding focus fetching
    
    * adding onclick

--- src/logs.jsx
+++ src/logs.jsx
@@ -2,59 +2,129 @@ import React from 'react';
 import { TrackedComponent } from 'react-appinsights';
 import GitHub from 'github-api';
 import ReactMarkdown from 'react-markdown';
+import { Collapse } from 'react-collapse';
+import { presets } from 'react-motion';
 import './logs.css';
 
+let repo;
 const user = 'dparkar';
 const repoName = 'blog-react';
 const repoBranch = 'master';
-const repoLogsFolder = 'content';
+const repoContentPath = 'content';
+const repoLogsMetadataFile = 'logs.json';
 
 export default class Logs extends TrackedComponent {
   constructor(props) {
     super(props);
     var gh = new GitHub();
     // get the repo
-    var repo = gh.getRepo(user, repoName);
-    // get the folder contents
-    repo.getContents(repoBranch, repoLogsFolder, true, (err1, files) => {
-      if (err1) {
-        console.log(err1); // we can't get the data, for some reason
-        return;
-      }
-      // go through each file in the folder
-      files.forEach(function(file) {
+    repo = gh.getRepo(user, repoName);
+    // get the logs metadata
+    repo.getContents(
+      repoBranch,
+      repoContentPath + '/' + repoLogsMetadataFile,
+      true,
+      (err, logs) => {
+        if (err) {
+          console.log(err); // we can't get the data, for some reason
+          return;
+        }
+        logs.forEach(function(log) {
+          log['selected'] = false;
+          log['content'] = '';
+        }, this);
+        logs[0]['selected'] = true;
         repo.getContents(
           repoBranch,
-          repoLogsFolder + '/' + file.name,
+          repoContentPath + '/' + logs[0].filename,
           true,
-          (err2, content) => {
-            if (err2) {
-              console.log(err2); // we can't have the data, for some reason
+          (err, content) => {
+            if (err) {
+              console.log(err); // we can't have the data, for some reason
               return;
             }
-            console.log(content);
-            this.setState({ logs: content });
+            logs[0].content = content;
+            this.setState({ logs: logs });
           }
         );
-      }, this);
-    });
-    // Update state
+        this.setState({ logs: logs });
+      }
+    );
+    // Intialize state
     this.state = {
-      //repoName: repoName,
-      id: 'fetching id ...',
-      logs: 'fetching logs ...'
+      logs: []
     };
   }
-
+  handleClick = e => {
+    var logIndex = this.state.logs.findIndex(
+      log => log.datetime === e.currentTarget.dataset.id
+    );
+    var logs = this.state.logs;
+    logs.forEach(function(log) {
+      log['selected'] = false;
+    }, this);
+    var log = this.state.logs[logIndex];
+    if (log.content === '') {
+      repo.getContents(
+        repoBranch,
+        repoContentPath + '/' + log.filename,
+        true,
+        (err, content) => {
+          if (err) {
+            console.log(err); // we can't have the data, for some reason
+            return;
+          }
+          log.content = content;
+          log.selected = true;
+          logs[logIndex] = log;
+          this.setState({ logs: logs });
+        }
+      );
+    } else {
+      log.selected = true;
+      logs[logIndex] = log;
+      this.setState({ logs: logs });
+    }
+  };
   render() {
+    let logTitles;
+    if (this.state.logs.length === 0) {
+      logTitles = <p>retrieving logs ...</p>;
+    } else {
+      logTitles = this.state.logs.map(log => {
+        if (log.selected) {
+          return (
+            <Collapse
+              key={log.datetime}
+              isOpened={true}
+              onClick={this.handleClick}
+              data-id={log.datetime}
+              springConfig={presets.wobbly}
+            >
+              {log.datetime + '_' + log.title} {'[' + log.tags + ']'}
+              <ReactMarkdown source={log.content} />
+            </Collapse>
+          );
+        } else {
+          return (
+            <Collapse
+              key={log.datetime}
+              isOpened={true}
+              onClick={this.handleClick}
+              data-id={log.datetime}
+              springConfig={presets.wobbly}
+            >
+              {log.datetime + '_' + log.title} {'[' + log.tags + ']'}
+            </Collapse>
+          );
+        }
+      });
+    }
+
     return (
       <div className="Logs">
-        <ReactMarkdown source={this.state.logs} />
+        {logTitles}
         <p>work in progress ...</p>
-        <img
-          src="https://i.makeagif.com/media/10-27-2015/_jDzHB.gif"
-          alt="work in progress"
-        />
         <p>
           See code here : {' '}
           <a href="https://github.com/dparkar/blog-react">
