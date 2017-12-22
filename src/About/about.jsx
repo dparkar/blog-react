38e9e65c4a3e5b291f3407263e68371ad0943ab8
Dhawal Parkar <dhawalparkar@gmail.com>
2017-12-22 01:50:54 -0800

    using metatags

--- src/About/about.jsx
+++ src/About/about.jsx
@@ -1,31 +1,40 @@
 import React from 'react';
 import { TrackedComponent } from 'react-appinsights';
 import FontAwesome from 'react-fontawesome';
+import MetaTags from 'react-meta-tags';
+
 import './about.css';
 
 export default class About extends TrackedComponent {
   render() {
     return (
       <div className="about">
-        <title> about | dhawal parkar </title>
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="dplogs" />
-        <meta property="og:title" content="about | dhawal parkar" />
-        <meta property="og:url" content="http://dplogs.com/about" />
-        <meta
-          property="og:image"
-          content="https://dplogscontent.blob.core.windows.net/dplogs/metaimage_250_250.png"
-        />
-        <meta property="og:image:alt" content="dplogs" />
-        <meta property="og:image:width" content="250" />
-        <meta property="og:image:height" content="250" />
-        <meta
-          property="og:description"
-          content="Artificial Intelligence and Robotics"
-        />
-        <meta property="fb:app_id" content="367989410291145" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="@dparkar" />
+        <MetaTags>
+          <title>about | dhawal parkar</title>
+          <meta id="1" name="description" content="Some description." />
+          <meta id="2" property="og:title" content="MyApp" />
+          <meta id="3" property="og:image" content="path/to/image.jpg" />
+          <meta id="4" property="og:type" content="website" />
+          <meta id="5" property="og:site_name" content="dplogs" />
+          <meta id="6" property="og:title" content="about | dhawal parkar" />
+          <meta id="7" property="og:url" content="http://dplogs.com/about" />
+          <meta
+            id="8"
+            property="og:image"
+            content="https://dplogscontent.blob.core.windows.net/dplogs/metaimage_250_250.png"
+          />
+          <meta id="9" property="og:image:alt" content="dplogs" />
+          <meta id="10" property="og:image:width" content="250" />
+          <meta id="11" property="og:image:height" content="250" />
+          <meta
+            id="12"
+            property="og:description"
+            content="Artificial Intelligence and Robotics"
+          />
+          <meta id="13" property="fb:app_id" content="367989410291145" />
+          <meta id="14" name="twitter:card" content="summary" />
+          <meta if="15" name="twitter:creator" content="@dparkar" />
+        </MetaTags>
         <div className="aboutcontent">
           <a
             href="https://www.linkedin.com/in/dparkar"
